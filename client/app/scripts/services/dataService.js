f82c57f5c9eb8f86bb98c61b4e2a6c2e04cad87f
wielki.borsuk <wielki.borsuk@gmail.com>
2013-11-09 01:05:12 +0100

    endpoint change, restyling, added multi browser support

--- client/app/scripts/services/dataService.js
+++ client/app/scripts/services/dataService.js
@@ -5,8 +5,8 @@ angular.module('clientApp')
     return {
       // endpoint_url: 'http://localhost:4400/',
      //  upload_base_url: 'http://localhost:4400/www/upload/',
-      endpoint_url: 'http://31.6.70.108:8080/player/',
-      upload_base_url: 'http://31.6.70.108:8080/www/upload/',
+      endpoint_url: 'http://31.6.70.108:80/player/',
+      upload_base_url: 'http://31.6.70.108:80/www/upload/',
       brak: ''
     }
   });
