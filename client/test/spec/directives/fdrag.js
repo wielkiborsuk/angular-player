159c2b359889a6abb827abf6c7f1674b36d3df1b
wielki.borsuk <wielki.borsuk@gmail.com>
2014-03-23 18:53:50 +0100

    New fDrag directive test, still in progress.

--- client/test/spec/directives/fdrag.js
+++ client/test/spec/directives/fdrag.js
@@ -6,15 +6,45 @@ describe('Directive: fDrag', function () {
   beforeEach(module('clientApp'));
 
   var element,
+    comp,
     scope;
 
-  beforeEach(inject(function ($rootScope) {
+  beforeEach(inject(function ($rootScope, $compile) {
     scope = $rootScope.$new();
+    comp = $compile;
   }));
 
-  it('should make hidden element visible', inject(function ($compile) {
- //   element = angular.element('<f-drag></f-drag>');
-  //  element = $compile(element)(scope);
-//    expect(element.text()).toBe('this is the fDrag directive');
-  }));
+  it('should add all drag-related attributes and binds to an object', function () {
+    expect(scope.hasOwnProperty('dragData')).toBe(false);
+    expect(scope.hasOwnProperty('dragStyle')).toBe(false);
+    element = angular.element('<div f-drag drag="el"></div>');
+    element = comp(element)(scope);
+
+    expect(element.attr('draggable')).toEqual('true');
+    expect(scope.hasOwnProperty('dragData')).toBe(true);
+    expect(scope.hasOwnProperty('dragStyle')).toBe(true);
+    expect(scope.dragData).toBeFalsy();
+    expect(scope.dragstyle).toBeFalsy();
+  });
+
+  it('should pass attribute values as parameters to directives scope', function () {
+    scope.el = 'hohoho';
+    expect(scope.hasOwnProperty('dragData')).toBe(false);
+    expect(scope.hasOwnProperty('dragStyle')).toBe(false);
+    element = angular.element('<div f-drag drag="el" dragstyle="highlight"></div>');
+    element = comp(element)(scope);
+
+    expect(scope.dragData).toEqual('hohoho');
+    expect(scope.dragStyle).toEqual('highlight');
+  });
+
+  it('should pass dragData to the dragstart event', function () {
+    scope.el = 'hohoho';
+    expect(scope.hasOwnProperty('dragData')).toBe(false);
+    expect(scope.hasOwnProperty('dragStyle')).toBe(false);
+    element = angular.element('<div f-drag drag="el" dragstyle="highlight"></div>');
+    element = comp(element)(scope);
+
+    element.triggerHandler('dragstart');
+  });
 });
