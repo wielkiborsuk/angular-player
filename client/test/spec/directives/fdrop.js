548a12bc4a7fdb9fc432ab2bdb431a0c87ab43f7
wielki.borsuk <wielki.borsuk@gmail.com>
2014-03-24 16:25:25 +0100

    Fdrop continued.

--- client/test/spec/directives/fdrop.js
+++ client/test/spec/directives/fdrop.js
@@ -6,15 +6,26 @@ describe('Directive: fDrop', function () {
   beforeEach(module('clientApp'));
 
   var element,
+    comp,
+    dragenterEvent,
+    dragleaveEvent,
+    dragoverEvent,
+    dropEvent,
     scope;
 
-  beforeEach(inject(function ($rootScope) {
+  beforeEach(inject(function ($rootScope, $compile) {
     scope = $rootScope.$new();
+    comp = $compile;
+
+    dragenterEvent = jQuery.Event('dragenter');
+    dragleaveEvent = jQuery.Event('dragleave');
+    dragoverEvent = jQuery.Event('dragover');
+    drop = jQuery.Event('drop');
   }));
 
-  it('should make hidden element visible', inject(function ($compile) {
+  it('should make hidden element visible', function () {
     //element = angular.element('<f-drop></f-drop>');
     //element = $compile(element)(scope);
     //expect(element.text()).toBe('this is the fDrop directive');
-  }));
+  });
 });
