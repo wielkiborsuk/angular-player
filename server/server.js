4f467265685a61294d3c498085725ddeb6863a46
wielki.borsuk <wielki.borsuk@gmail.com>
2014-08-05 21:02:49 +0200

    refactor out the server part of the project. into a separate repo

--- server/server.js
+++
@@ -1,9 +0,0 @@
-var http = require('http'),
-	https = require('https'),
-	app = require('./app')
-
-var port = process.env.PORT || 4400
-
-http.createServer(app).listen(port, function () {
-	console.log("Express server listening on " + port)
-})
