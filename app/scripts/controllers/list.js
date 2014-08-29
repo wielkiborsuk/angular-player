ecf4aae635ab1f860a01e4c0e9e2b8f863e767d7
wielki.borsuk <wielki.borsuk@gmail.com>
2014-08-29 08:03:47 +0200

    copy/paste original list manipulation - needs review

--- app/scripts/controllers/list.js
+++ app/scripts/controllers/list.js
@@ -28,4 +28,62 @@ angular.module('angularPlayerApp')
       $scope.state.active_type = $scope.state.listview;
       $scope.state.song_queue.splice(0, $scope.state.song_queue.length);
     };
+
+    $scope.rescan = function () {
+      Listsservice.smscan().then(function (/*res*/) {
+        Listsservice.scanned().then(function (res) {
+          $scope.state.mediadirs = res.data;
+        });
+      });
+    };
+
+    $scope.list_add = function () {
+      if ($scope.new_list.name) {
+        var tmp_list = $scope.new_list;
+        $scope.new_list = {};
+        tmp_list.files = [];
+        $scope.lists.push(tmp_list);
+        listsService.list_create(tmp_list).success(function (res) {
+          tmp_list._id = res;
+        });
+      } else {
+        console.log('Cant insert a list with no name.');
+      }
+    };
+
+    $scope.list_delete = function(li, $event) {
+      $event.stopPropagation();
+      if (li) {
+        if (li._id) {
+          listsService.list_delete(li._id).success(function(/*res*/) {
+            arr_del($scope.lists, li);
+          });
+        } else {
+          arr_del($scope.lists, li);
+        }
+      }
+    };
+
+    $scope.list_push = function(li, f) {
+      if ($scope.edit && li && li._id && li.files.indexOf(f)<0) {
+        li.files.push(f);
+        listsService.list_put(li._id, li);
+      }
+    };
+
+    $scope.list_pop = function(li, f/*, $event*/) {
+      if (li && f) {
+        arr_del(li.files, f);
+        if (li._id) {
+          listsService.list_put(li._id, li);
+        }
+      }
+    };
+
+    function arr_del(arr, obj) {
+      var idx = arr.indexOf(obj);
+      if (idx > -1) {
+        arr.splice(idx, 1);
+      }
+    }
   });
